574d80c9ced9e2dad222d2684f25165fbee4845a
emyzinger <33734235+emyzinger@users.noreply.github.com>
2018-08-01 13:55:27 -0500

    Methods are in pseudocode

--- src/WordSorter.java
+++ src/WordSorter.java
@@ -1,3 +1,22 @@
 public class WordSorter {
 
+    // make an array containing 26 sets
+    private words = new array[];
+
+    private void importDictionary(){
+        //import the dictionary, then call next method
+    }
+
+    private void getWord (){
+        //go though dictionary putting words into the array
+        //call method that goes though word and puts it into the array
+    }
+
+    private void putWord(){
+        //go through word letter by letter and put it into the array ASCII - 65 i think
+    }
+
+    public array getArray{
+        return words;
+    }
 }
