c68037b53eecf271eaaccba2ff40568a7ed73c3a
Emilia Zinger <emzinger@hotmail.com>
2018-08-03 21:53:40 -0500

    finished the class, just needs to be tested

--- src/WordSorter.java
+++ src/WordSorter.java
@@ -1,42 +1,75 @@
+import java.io.*;
+import java.util.*;
+
 public class WordSorter {
 
     // make an array containing 26 sets
-    private words = new array[];
+    private List<Set<String>> words = new ArrayList<>();
     Scanner dict = null;
 
-    private void importDictionary(){
-        //import the dictionary, then call next method
-        try
-        {
-            dict = new Scanner (new File (words_alpha.txt));
-        }
-        catch(FileNotFoundException e)
-        {
-            System.out.println ("Error opening the file " +
-                    words_alpha.txt );
-            System.exit (0);
-        }
-        dict.close ();
-        getWord();//the document to read. each word is on a new line
+    public WordSorter(String fileName){
+        //will be called from WordFiner
+        init();
+        importDictionary(fileName);
     }
 
-    private void getWord (){
-        //go though dictionary putting words into the array
-        //call method that goes though word and puts it into the array
-        while (dict.hasNextLine){
-            word = dict.nextLine.parseString();
-            putWord(word);
+    private void init(){
+        //makes the array have 26 elements
+        for (int i = 0; i<26; i++){
+            words.add(new HashSet<>());
+        }
+    }
+
+    private void importDictionary(String fileName){
+        //import the dictionary, then call next method
+        // Code in this method taken from internet and edited
+        //https://www.caveofprogramming.com/java/java-file-reading-and-writing-files-in-java.html
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                line = line.toUpperCase();
+                putWord(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
         }
     }
 
     private void putWord(String word){
-        //go through word letter by letter and put it into the array ASCII - 65 i think
-        for(i = 0; i < word; i++ ){
-
+        //go through word letter by letter and put it into the array ASCII - 65
+        for(int i = 0; i < word.length(); i++ ){
+           char letter =  word.charAt(i);
+           int ascii = (int) letter;
+           int index = ascii - 65;
+           words.get(index).add(word);
         }
     }
 
-    public array getArray{
+    public List getArray(){
         return words;
     }
 }
