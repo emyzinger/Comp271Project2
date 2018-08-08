f94162bae2b8e673523e633904dead0f5abe9e2d
pbhatt33 <pbhatt3@luc.edu>
2018-08-08 16:51:45 -0500

    need more tests

--- src/WordFinder.java
+++ src/WordFinder.java
@@ -17,12 +17,25 @@ public class WordFinder{
 
     private void findWords(String word, String fileName) {
         WordSorter wordSort = new WordSorter(fileName);
-        List<Set<String>> allWords=wordSort.getArray();
-        answers=allWords.get(((int) word.charAt(0))-65);
-        for(int i=1;i<word.length();i++){
-            answers.retainAll(allWords.get(((int) word.charAt(i))-65));
+        List<Set<String>> allWords = wordSort.getArray();
+        answers = allWords.get(((int) word.charAt(0)) - 65);
+        for (int i = 1; i < word.length(); i++) {
+            answers.retainAll(allWords.get(((int) word.charAt(i)) - 65));
         }
-        
+
+
+        Iterator<String> iterator = answers.iterator();
+        Set<String> answers2=new HashSet<>();
+        while (iterator.hasNext()) {
+            String current = iterator.next();
+            for(int i=0;i<word.length()-1;i++){
+                if (current.indexOf(word.charAt(i)) > current.indexOf(word.charAt(i+1))) {
+                    answers2.add(current);
+                }
+
+            }
+        }
+        answers.removeAll(answers2);
     }
 }
 
